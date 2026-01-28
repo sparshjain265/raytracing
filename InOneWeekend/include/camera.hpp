1514b4172f819c0afb7a7e92c3b8f8af793f266a
Sparsh Jain <sparshjain.265@gmail.com>
2026-01-27 18:04:30 -0800

    refactoring by adding a camera module

---
+++ InOneWeekend/include/camera.hpp
@@ -0,0 +1,123 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2026 Sparsh Jain
+ *
+ */
+
+#ifndef INONEWEEKEND_INCLUDE_CAMERA_HPP
+#define INONEWEEKEND_INCLUDE_CAMERA_HPP
+
+#include <concepts>
+
+#include "hittable.hpp"
+#include "color.hpp"
+#include "ray.hpp"
+
+template <std::floating_point T = double>
+class Camera
+{
+public:
+    constexpr Camera() = default;
+
+    constexpr T aspectRatio() const { return m_aspectRatio; }
+    constexpr int imageWidth() const { return m_imageWidth; }
+
+    void setAspectRatio(T aspectRatio)
+    {
+        m_aspectRatio = aspectRatio;
+    }
+
+    void setImageWidth(int imageWidth)
+    {
+        m_imageWidth = imageWidth;
+    }
+
+    void render(const Hittable<T> &world)
+    {
+        // Always initialize before rendering
+        initialize();
+
+        std::cout << "P3\n"
+                  << m_imageWidth << ' ' << m_imageHeight << "\n255\n";
+
+        for (int i = 0; i < m_imageHeight; ++i)
+        {
+            std::clog << "\rScanlines remaining: " << (m_imageHeight - i) << ' ' << std::flush;
+            for (int j = 0; j < m_imageWidth; ++j)
+            {
+                const auto pixelCenter = m_pixel00Center +
+                                         i * m_pixelDeltaVertical +
+                                         j * m_pixelDeltaHorizontal;
+
+                const auto rayDirection = pixelCenter - m_center;
+                const Ray<T> ray(m_center, rayDirection);
+                const auto pixelColor = rayColor(ray, world);
+
+                writeColor(std::cout, pixelColor);
+            }
+        }
+
+        std::clog << "\rDone.                                              \n";
+    }
+
+private:
+    // Publically Accessible Camera Parameters
+    T m_aspectRatio{1.0};
+    int m_imageWidth{100};
+
+    // Internally Used Camera Parameters
+    int m_imageHeight{100};
+    Point3<T> m_center{};
+    Point3<T> m_pixel00Center{};
+    Vector3<T> m_pixelDeltaHorizontal{};
+    Vector3<T> m_pixelDeltaVertical{};
+
+    void initialize()
+    {
+        m_imageHeight = static_cast<int>(m_imageWidth / m_aspectRatio);
+        m_imageHeight = (m_imageHeight < 1) ? 1 : m_imageHeight;
+
+        m_center = Point3<T>(0.0, 0.0, 0.0);
+
+        // Viewport Setup
+        const T focalLength = 1.0;
+        const T viewportHeight = 2.0;
+        const T viewportWidth = viewportHeight * (static_cast<T>(m_imageWidth) / m_imageHeight);
+
+        const auto viewportHorizontal = Vector3<T>(viewportWidth, 0.0, 0.0);
+        const auto viewportVertical = Vector3<T>(0.0, -viewportHeight, 0.0);
+
+        m_pixelDeltaHorizontal = viewportHorizontal / m_imageWidth;
+        m_pixelDeltaVertical = viewportVertical / m_imageHeight;
+
+        const auto viewportTopLeft = m_center -
+                                     Vector3<T>(0.0, 0.0, focalLength) -
+                                     (viewportHorizontal / 2) -
+                                     (viewportVertical / 2);
+
+        m_pixel00Center = viewportTopLeft + 0.5 * (m_pixelDeltaHorizontal + m_pixelDeltaVertical);
+    }
+
+    constexpr Color<T> rayColor(const Ray<T> &r, const Hittable<T> &world)
+    {
+        HitRecord<T> record;
+
+        if (world.hit(r, Interval<T>(static_cast<T>(0), infinity<T>), record))
+        {
+            const auto N = record.normal();
+            constexpr auto ones = Color<T>(1.0, 1.0, 1.0);
+            return static_cast<T>(0.5) * (ones + N);
+        }
+
+        const auto unitDirection = unitVector(r.direction());
+        const T t = static_cast<T>(0.5) * (unitDirection.y() + static_cast<T>(1.0));
+
+        constexpr auto white = Color<T>(1.0, 1.0, 1.0);
+        constexpr auto blue = Color<T>(0.5, 0.7, 1.0);
+
+        return (1 - t) * white + t * blue;
+    }
+};
+
+#endif /* INONEWEEKEND_INCLUDE_CAMERA_HPP */
--- InOneWeekend/main.cpp
+++ InOneWeekend/main.cpp
@@ -17,88 +17,23 @@
 #include "hittable_list.hpp"
 #include "sphere.hpp"
 #include "interval.hpp"
-
-template <std::floating_point T = double>
-constexpr Color<T> rayColor(const Ray<T> &r, const Hittable<T> &world)
-{
-    HitRecord<T> record;
-    if (world.hit(r, Interval<T>(static_cast<T>(0), infinity<T>), record))
-    {
-        const auto N = record.normal();
-        constexpr auto ones = Color<T>(1.0, 1.0, 1.0);
-        return static_cast<T>(0.5) * (ones + N);
-    }
-
-    const auto unitDirection = unitVector(r.direction());
-    const T t = static_cast<T>(0.5) * (unitDirection.y() + static_cast<T>(1.0));
-
-    constexpr auto white = Color<T>(1.0, 1.0, 1.0);
-    constexpr auto blue = Color<T>(0.5, 0.7, 1.0);
-
-    return (1 - t) * white + t * blue;
-}
+#include "camera.hpp"
 
 int main()
 {
-    // Image Setup
-    constexpr auto idealAspectRatio = 16.0 / 9.0;
-    constexpr int imageWidth = 400;
-    constexpr int idealImageHeight = static_cast<int>(imageWidth / idealAspectRatio);
-    constexpr int imageHeight = (idealImageHeight < 1) ? 1 : idealImageHeight;
-    constexpr auto aspectRatio = static_cast<double>(imageWidth) / imageHeight;
-
-    // Camera Setup
-    constexpr auto focalLength = 1.0;
-    constexpr auto cameraCenter = Point3(0.0, 0.0, 0.0);
-    constexpr auto viewportHeight = 2.0;
-    constexpr auto viewportWidth = viewportHeight * aspectRatio;
-
-    // Viewport Vectors
-    constexpr auto viewportHorizontal = Vector3(viewportWidth, 0.0, 0.0);
-    constexpr auto viewportVertical = Vector3(0.0, -viewportHeight, 0.0);
-
-    // Pixel Delta Vectors
-    constexpr auto pixelDeltaHorizontal = viewportHorizontal / imageWidth;
-    constexpr auto pixelDeltaVertical = viewportVertical / imageHeight;
-
-    // Viewport Top-Left Corner
-    constexpr auto viewportTopLeft = cameraCenter -
-                                     Vector3(0.0, 0.0, focalLength) -
-                                     (viewportHorizontal / 2.0) -
-                                     (viewportVertical / 2.0);
-
-    // Pixel 00 Center
-    constexpr auto pixel00Center = viewportTopLeft +
-                                   0.5 * (pixelDeltaHorizontal + pixelDeltaVertical);
+    using T = double;
 
     // World Setup
-    HittableList<double> world;
+    HittableList<T> world;
 
-    world.add(std::make_shared<Sphere<double>>(Point3(0.0, 0.0, -1.0), 0.5));
-    world.add(std::make_shared<Sphere<double>>(Point3(0.0, -100.5, -1.0), 100.0));
+    world.add(std::make_shared<Sphere<T>>(Point3<T>(0.0, 0.0, -1.0), 0.5));
+    world.add(std::make_shared<Sphere<T>>(Point3<T>(0.0, -100.5, -1.0), 100.0));
 
-    // Render
-    std::cout << "P3\n"
-              << imageWidth << ' ' << imageHeight << "\n255\n";
+    Camera<T> camera;
+    camera.setAspectRatio(16.0 / 9.0);
+    camera.setImageWidth(400);
 
-    for (int i = 0; i < imageHeight; ++i)
-    {
-        std::clog << "\rScanlines remaining: " << imageHeight - i << ' ' << std::flush;
-        for (int j = 0; j < imageWidth; ++j)
-        {
-            auto pixelCenter = pixel00Center +
-                               i * pixelDeltaVertical +
-                               j * pixelDeltaHorizontal;
-
-            auto rayDirection = pixelCenter - cameraCenter;
-            Ray ray(cameraCenter, rayDirection);
-
-            auto pixelColor = rayColor(ray, world);
-            writeColor(std::cout, pixelColor);
-        }
-    }
-
-    std::clog << "\rDone.                                              \n";
+    camera.render(world);
 
     return 0;
 }
